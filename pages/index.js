6b6786c0c8c0ac1c2dab8baff7dc66a3d9060da5
AffirmedVisionary <iam@charlenegrant.co.uk>
2021-04-14 09:57:40 +0100

    implement hero and features on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Articles from "../components/articles";
+import Feature from "../components/feature";
+// import Hero from "../components/hero";
+import Hero1 from "../components/hero1";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { fetchAPI } from "../lib/api";
@@ -8,11 +11,12 @@ const Home = ({ articles, categories, homepage }) => {
   return (
     <Layout categories={categories}>
       <Seo seo={homepage.seo} />
-      <div className="uk-section">
-        <div className="uk-container uk-container-large">
-          <h1>{homepage.hero.title}</h1>
+      <div className="uk-section" style={{paddingTop: '0'}}>
+        <Hero1 data={homepage.hero} />
+        <Feature data={homepage.feature} iconlist={homepage.featlist}/>
+        <div id='for-articles'>
           <Articles articles={articles} />
-        </div>
+          </div>
       </div>
     </Layout>
   );
